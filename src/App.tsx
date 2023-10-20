8ff8a5f4c3dc85047619121a84094a8076da3a95
Lambertn33 <lambertgithub@gmail.com>
2023-10-20 12:28:59 +0200

    feat: move navbar to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,36 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { Container, Row, Col } from "react-bootstrap";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { Container, Row, Col, Nav, Navbar } from "react-bootstrap";
 
 import { NewsPaper, NewsPapers } from "./pages/newspapers";
 import { Publisher, Publishers } from "./pages/publishers";
 import Home from "./pages/Home";
 
-import {Header as MainHeader} from "./components/header";
+import newsLettersImage from './assets/newsletters.png';
 
-import styles from './App.module.css'
+import styles from './App.module.css';
 
 const App = () => {
   return (
     <Router>
-      <MainHeader />
+      
+      {/* Navbar */}
+      <Navbar expand="lg" bg="primary" data-bs-theme="light">
+      <Container className={styles.nav_container}>
+        <Navbar.Brand href="/" className={styles.navbar_brand}>
+          <img src={newsLettersImage} alt="" width={40}/>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className={`me-auto ${styles.navbar_nav}`}>
+            <Link to="/">Home</Link>
+            <Link to="/publishers">Publishers</Link>
+            <Link to="/newspapers">NewsPapers</Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+    {/* Navbar */}
+
       <Container className={styles.container}>
         <Row>
           <Col>
