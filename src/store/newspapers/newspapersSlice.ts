b0b84ee1baba7d91da3fc95771fa99347a2f383e
Lambertn33 <lambertgithub@gmail.com>
2023-10-20 10:11:44 +0200

    chore: clean

--- src/store/newspapers/newspapersSlice.ts
+++ src/store/newspapers/newspapersSlice.ts
@@ -1,4 +1,4 @@
-import { createSlice, createAsyncThunk, PayloadAction } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import { getNewsPapers } from "../../api/api";
 
 export interface NewsPaper {
